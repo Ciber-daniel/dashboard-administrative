7c1bc147995a86c597fec275f3b7683cc973a853
qjordnvb <jordanvillarreal1994@gmail.com>
2021-03-09 13:34:14 -0500

    WIP

--- src/components/list/List-Drawer.js
+++ src/components/list/List-Drawer.js
@@ -13,6 +13,9 @@ import ExpandMore from "@material-ui/icons/ExpandMore";
 import IconDashboard from "../../assets/img/icon-dashboard.svg";
 import IconUsers from "../../assets/img/icon-users.svg";
 import IconTables from "../../assets/img/icon-tables.svg";
+import IconPolizas1 from "../../assets/img/icon-polizas-aprobadas.svg";
+import IconPolizas2 from "../../assets/img/icon-polizas-onboarding.svg";
+import ArrowDown from "../../assets/img/arrow-down.svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -66,12 +69,28 @@ export default function ListDrawer() {
         </ListItemIcon>
         <p className={classes.textColor}>Usuarios Asegurados</p>
       </ListItem>
+      <ListItem button className={classes.itemList}>
+        <ListItemIcon>
+          <img className={classes.iconsDrawer} src={IconPolizas1} />
+        </ListItemIcon>
+        <p className={classes.textColor}>Pólizas aprobadas</p>
+      </ListItem>
+      <ListItem button className={classes.itemList}>
+        <ListItemIcon>
+          <img className={classes.iconsDrawer} src={IconPolizas2} />
+        </ListItemIcon>
+        <p className={classes.textColor}>Pólizas en Onboarding</p>
+      </ListItem>
       <ListItem button className={classes.itemList} onClick={handleClick}>
         <ListItemIcon>
           <img className={classes.iconsDrawer} src={IconTables} />
         </ListItemIcon>
         <p className={classes.textColor}>Tablas</p>
-        {open ? <ExpandLess /> : <ExpandMore />}
+        {open ? (
+          <img src={ArrowDown} className="icon-expand" />
+        ) : (
+          <img src={ArrowDown} className="icon-expand" />
+        )}
       </ListItem>
       <Collapse in={open} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
